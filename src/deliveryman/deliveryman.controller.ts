7dd6dbef89cda1515c10a26a2352c6834121aa5a
claude17 <claude.mta@gmail.com>
2024-03-22 12:27:39 +0600

    Update deliveryman.controller.ts

--- src/deliveryman/deliveryman.controller.ts
+++ src/deliveryman/deliveryman.controller.ts
@@ -1,7 +1,8 @@
 
-import { Body, Controller, Get, Param, Post, Query, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Put, Post, Delete, Query, UsePipes, ValidationPipe } from "@nestjs/common";
 import { DeliverymanService } from "./deliveryman.service";
 import { DeliverymanDTO } from "./deliveryman.dto";
+import { DeliverymanEntity } from './deliveryman.entity';
 
 
 
@@ -21,4 +22,20 @@ export class DeliverymanController{
     getUserss() {
         return this.deliverymanService.getUserss();
     }
+    @Post('createuser')
+    async createUser(@Body() user: DeliverymanEntity): Promise<DeliverymanEntity> {
+    return this.deliverymanService.createUser(user);
+  }
+  @Put('updateUser/:id')
+  async updateUser(@Param('id') id: number, @Body() updatedUser: DeliverymanEntity): Promise<DeliverymanEntity> {
+    return this.deliverymanService.updateUser(id, updatedUser);
+  }
+  @Get('usersWithNullFullName')
+  async getUsersWithNullFullName(): Promise<DeliverymanEntity[]> {
+    return this.deliverymanService.getUsersWithNullFullName();
+  }
+  @Delete('deleteUser/:id')
+  async deleteUser(@Param('id') id: number): Promise<string> {
+    return this.deliverymanService.deleteUser(id);
+  }
 }
